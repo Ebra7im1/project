08b7fac8847bdf2917c2ca72f7f7970ffa99c223
Ebra7im1 <121352963+Ebra7im1@users.noreply.github.com>
2022-12-25 15:31:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, sum;
+            int an, ax;
+
+            Console.Write("Find the perfect number between two numbers:\n");
+
+            Console.Write("\n\n");
+
+            Console.Write("Enter First Number : ");
+            an = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number : ");
+            ax = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The perfect number between the two numbers is : ");
+            for (n = an; n <= ax; n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.Write("{0} ", n);
+            }
+            Console.Write("\n");
+
+        }
+    }
+}
